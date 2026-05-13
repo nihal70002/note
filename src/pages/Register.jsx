e0165ad6f887e44fbd791cabab94de5f770393fc
nihal70002 <nihalahd7000@gmail.com>
2026-05-13 20:42:20 +0530

    djkbasjdh

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -21,7 +21,7 @@ const Register = () => {
       const success = await register(phoneNumber, password);
       if (success) {
         showToast('success', 'Registration successful. Please sign in.');
-        navigate('/shop');
+        navigate('/');
       }
     } finally {
       setLoading(false);
